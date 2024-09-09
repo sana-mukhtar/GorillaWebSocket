ae7fbd737ad633438d05a2662a642e256fd86615
Sana <sanamukhtar969@gmail.com>
2024-09-09 23:08:25 +0530

    write basic server syntax

--- server/server.go
+++ server/server.go
@@ -1,5 +1,39 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func handlerFunc(w http.ResponseWriter, req *http.Request) {
+	conn, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	for {
+		msgType, msg, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("err readMessage: ", err)
+			break
+		}
+
+		if err := conn.WriteMessage(msgType, msg); err != nil {
+			log.Println("err writeMessage: ", err)
+			break
+		}
+	}
+}
+
 func main() {
-	
+	http.HandleFunc("/ws", handlerFunc)
+	addr := ":8080"
+	http.ListenAndServe(addr, nil)
 }
